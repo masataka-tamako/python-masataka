7f6040ef67274abdc11fc3476636057ca6a64f10
masataka <masataka1025h@gmail.com>
2022-02-11 14:17:49 +0900

    conditional branch

---
+++ 条件分岐.py
@@ -0,0 +1,22 @@
+input_member = input("会員の場合はYを入れてください: ")
+input_age = input("何歳ですか？: ")
+
+#年齢を入力値を整数に変換
+age = int(input_age)
+
+#会員と非会員で分岐処理
+if input_member == "Y":
+    if age < 18:
+        price = 100
+    
+    else:
+        price = 1000
+
+else:
+    if age < 18:
+        price = 1000
+
+    else:
+        price = 2000
+
+print("料金は: ", price)
